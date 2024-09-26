802e4be45b5b83c92fff3117d35c11f4704312fa
Silmaris <pksiazyk@blueaccelerator.com>
2024-09-26 16:31:33 +0200

    commit z dokumentacją nt. funkcji

--- PalindromeDetector.py
+++ PalindromeDetector.py
@@ -1,9 +1,11 @@
 def detect_palindrome (word):
+    """funkcja usuwa spacje, zmniejsza litery, odwraca kolejność liter w wyrazie i porównuje z wyrazem podanym przez uzytkownika"""
     word = word.replace(" ", "").lower()
     word_reversed = word[::-1]
     if(word != ""):
         print(word == word_reversed)
 
+help(detect_palindrome)
 while True:    
     print("Podaj wyraz: ")
     word = input()
